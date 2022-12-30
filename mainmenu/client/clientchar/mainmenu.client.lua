19c1f48bc795afdb4d323576a6c0d51d9172b679
Raymond Brown <daimyoplays@gmail.com>
2022-12-29 23:58:14 -0600

    camera and build creations

--- mainmenu/client/clientchar/mainmenu.client.lua
+++ mainmenu/client/clientchar/mainmenu.client.lua
@@ -1,8 +1,7 @@
 local player = game.Players.LocalPlayer
 local character = player.Character
 local StarterGui = game:GetService("StarterGui")
-local PlayerGui = game:GetService("PlayerGui")
-local buildCreationButton = PlayerGui.MainMenu["Menu Frame"].BuildCreation
+local buildCreationButton = player:WaitForChild("PlayerGui"):WaitForChild("MainMenu")["Menu Frame"].BuildCreation
 local startBuildServer = game.ReplicatedStorage.StartBuildServer
 
 StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, false)
--- mainmenu/server/createprivateserver.server.lua
+++ mainmenu/server/createprivateserver.server.lua
@@ -1,10 +1,13 @@
 local startBuildServer = game.ReplicatedStorage.StartBuildServer
 local TeleportService = game:GetService("TeleportService") 
 
-local function createBuildServer(player) then
+local function createBuildServer(player)
     if player.UserId then
-        TeleportService:ReserveServer(11960320745)
-        local code = player.UserId
-        TeleportService:TeleportToPrivateServer(11960320745, code, player)
+        local code = TeleportService:ReserveServer(11960320745)
+        local TeleportOptions = Instance.new("TeleportOptions")
+        TeleportOptions.ReservedServerAccessCode = code
+        TeleportService:TeleportAsync(11960320745, {player}, TeleportOptions)
     end
 end
+
+startBuildServer.OnServerEvent:Connect(createBuildServer)
